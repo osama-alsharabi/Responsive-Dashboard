56e3130453fac5e45699ec6db8222850e1dad161
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-05 07:40:34 +0300

    adding drawer item as a model and custom widget and adding the the list of the item in the custom drawer

---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class DrawerItem extends StatelessWidget {
+  final DrawerItemModel drawerItemModel;
+  const DrawerItem({super.key, required this.drawerItemModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(drawerItemModel.title, style: AppStyles.textStyle16Regular),
+    );
+  }
+}
