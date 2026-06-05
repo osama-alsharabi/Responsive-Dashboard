f63cf4eea8a4c75d588ba9279e90cd108c23db08
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-05 06:28:24 +0300

    adding the assets of the dashboard and generate the app assets file by the plugin flutter assets gen

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for iconsBalance
+  /// assets/icons/balance.svg
+  static const String iconsBalance = "assets/icons/balance.svg";
+
+  /// Assets for iconsDashboard
+  /// assets/icons/dashboard.svg
+  static const String iconsDashboard = "assets/icons/dashboard.svg";
+
+  /// Assets for iconsExpenses
+  /// assets/icons/expenses.svg
+  static const String iconsExpenses = "assets/icons/expenses.svg";
+
+  /// Assets for iconsIncome
+  /// assets/icons/income.svg
+  static const String iconsIncome = "assets/icons/income.svg";
+
+  /// Assets for iconsLogout
+  /// assets/icons/logout.svg
+  static const String iconsLogout = "assets/icons/logout.svg";
+
+  /// Assets for iconsMyInvestments
+  /// assets/icons/my_investments.svg
+  static const String iconsMyInvestments = "assets/icons/my_investments.svg";
+
+  /// Assets for iconsMyTransaction
+  /// assets/icons/my_transaction.svg
+  static const String iconsMyTransaction = "assets/icons/my_transaction.svg";
+
+  /// Assets for iconsSetting
+  /// assets/icons/setting.svg
+  static const String iconsSetting = "assets/icons/setting.svg";
+
+  /// Assets for iconsStatistics
+  /// assets/icons/statistics.svg
+  static const String iconsStatistics = "assets/icons/statistics.svg";
+
+  /// Assets for iconsWalletAccount
+  /// assets/icons/wallet_account.svg
+  static const String iconsWalletAccount = "assets/icons/wallet_account.svg";
+
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesCardMask
+  /// assets/images/card_mask.svg
+  static const String imagesCardMask = "assets/images/card_mask.svg";
+}
+
