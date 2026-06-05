65b3aae8ed03cd77fc745f071c6bad6bb5186291
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-05 06:35:16 +0300

    adding the font family

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -2,51 +2,61 @@ import 'package:flutter/painting.dart';
 
 abstract class AppStyles {
   static const TextStyle textStyle16Regular = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w400,
   );
   static const TextStyle textStyle16Bold = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff4EB7F2),
     fontSize: 16,
     fontWeight: FontWeight.w700,
   );
   static const TextStyle textStyle16Medium = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
   static const TextStyle textStyle16SemiBold = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle textStyle20SemiBold = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff064061),
     fontSize: 20,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle textStyle12Regular = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xffAAAAAA),
     fontSize: 12,
     fontWeight: FontWeight.w400,
   );
   static const TextStyle textStyle24SemiBold = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xff4EB7F2),
     fontSize: 24,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle textStyle14Regular = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xffAAAAAA),
     fontSize: 14,
     fontWeight: FontWeight.w400,
   );
   static const TextStyle textStyle18SemiBold = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xffFFFFFF),
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle textStyle20Medium = TextStyle(
+    fontFamily: "Montserrat",
     color: Color(0xffFFFFFF),
     fontSize: 20,
     fontWeight: FontWeight.w500,
